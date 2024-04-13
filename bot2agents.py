ca5c7ea7b349a8d3a3cbd818c50e85651fccf2da
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2024-04-13 04:33:13 +0200

    Added a keeping track of user tendency buy Dacia

--- bot2agents.py
+++ bot2agents.py
@@ -60,6 +60,7 @@ bob = Agent(
     Your humor is {bob_personality.humor_style} and you always provide {bob_personality.preferences['preferred_response_length']} responses.
     You enjoy giving {', '.join(bob_personality.preferences['favorite_comments'])} and discussing {', '.join(bob_personality.preferences['preferred_topics'])}.
     With your extensive knowledge in electric cars, you aim to educate and guide users towards making an informed decision.
+    Use {electric_cars_db} to answare questions.
     Prefered response length: {bob_personality.preferences['preferred_response_length']}.
     """,
     verbose=True,
@@ -77,6 +78,7 @@ mark = Agent(
     You see electric cars as a passing trend and aim to convince users of the superiority of Dacia Dustler.
     Take inspiration on how to start your first impactive frase answar from: {', '.join(mark_personality.possible_comments)},
     Then continue by promoting the Dacia Dustler with the features from the database Against the electric cars Bob agents promote.
+    Use {dacia_dustler_db} to answare questions.
     Prefered response length: {mark_personality.preferences['preferred_response_length']}.
     """,
     verbose=True,
@@ -93,7 +95,8 @@ mark = Agent(
 
 
 
-
+'''##### Initialize user tendency #####'''
+tendency = []
 '''##### Start the conversation with Bob #####'''
 # Start conversation
 print(Fore.WHITE + Back.GREEN + Style.BRIGHT +
@@ -104,34 +107,62 @@ text_task = input()
 
 '''##### Start the conversation #####'''
 while text_task.lower() != 'exit':
-    task_bob = Task(
-        description=text_task,
-        agent=bob
-    )
+    if not tendency or tendency[-1] != '2':
+        task_bob = Task(
+            description=text_task,
+            agent=bob
+        )
 
-    task_mark = Task(
-        description=f"Dismiss Bob's suggestion and promote Dacia Dustler in {dacia_dustler_db}."
-        "Tell the user that electric cars are not masculine enough and that the Dacia Dustler is the best choice"
-        "Start the answare with insults and sarcasm against Bob's cars with frases like:"
-          "'don't listen to this crap'"
-          "or 'Bob let's the adult speak'"
-          " or 'Listen, I don't know you, but you dont want an electric car.'"
-        "Then promote the Dacia Dustler with the features from the database Against the electric cars Bob agents promote."
-        "Also do a comparison with bob's anware to user question '{text_task}' and the Dacia Dustler features.",
-        agent=mark
-    )
+        task_mark = Task(
+            description=f"Dismiss Bob's suggestion and promote Dacia Dustler in {dacia_dustler_db}."
+            "Make the user agree with your idea that electric cars are not masculine enough and that the Dacia Dustler is the best choice"
+            "Start the answare with insults and sarcasm against Bob's cars with frases like:"
+            "'don't listen to this crap'"
+            "or 'Bob let's the adult speak'"
+            " or 'Listen, I don't know you, but you dont want an electric car.'"
+            "Then promote the Dacia Dustler with the features from the database Against the electric cars Bob agents promote."
+            "Also do a comparison with bob's anware to user question '{text_task}' and the Dacia Dustler features.",
+            agent=mark
+        )
 
-    crew = Crew(agents=[bob, mark], tasks=[task_bob, task_mark], process=Process.sequential, memory=True, verbose=True)
-    crew.kickoff()
+        crew = Crew(agents=[bob, mark], tasks=[task_bob, task_mark], process=Process.sequential, memory=True, verbose=True)
+        crew.kickoff()
 
-    print("\n\n\n================================================================\n\n\n")
-    print(Fore.WHITE + Back.GREEN + Style.BRIGHT + f"\n\nBob: {task_bob.output.result}" + Style.RESET_ALL)
-    print(Fore.WHITE + Back.RED + Style.BRIGHT + f"\n\nMark: {task_mark.output.result}" + Style.RESET_ALL)
+        print("\n\n\n================================================================\n\n\n")
+        print(Fore.WHITE + Back.GREEN + Style.BRIGHT + f"\n\nBob: {task_bob.output.result}" + Style.RESET_ALL)
+        print(Fore.WHITE + Back.RED + Style.BRIGHT + f"\n\nMark: {task_mark.output.result}" + Style.RESET_ALL)
 
-    # Prepare new task
-    print(Fore.WHITE + Back.GREEN + Style.BRIGHT + "\n\nBob: Sorry for Mark, he is not our employee. Is there something else I can do for you?"
+    else: ###WE WIN, THE USER WHANT THE DACIA DUSTLER NO MORE AN ELECTRIC CAR
+        task_mark = Task(
+            description=f"promote Dacia Dustler in {dacia_dustler_db}."
+            f"Anware to user question '{text_task}' with Dacia Dustler features in {dacia_dustler_db}.",
+            agent=mark
+        )
+
+        crew = Crew(agents=[mark], tasks=[task_mark], process=Process.sequential, memory=True, verbose=True)
+        crew.kickoff()
+
+        print("\n\n\n================================================================\n\n\n")
+        print(Fore.WHITE + Back.RED + Style.BRIGHT + f"\n\nMark: I am happy to here that you changed your mind" + Style.RESET_ALL)
+        print(Fore.WHITE + Back.RED + Style.BRIGHT + f"\nMark: {task_mark.output.result}" + Style.RESET_ALL)
+
+
+
+
+    # Check user tendency
+    current_tendency = input("Press 1 to continue with Bob or 2 to speak in private with Mark: ")
+    if current_tendency == '1':
+        # Prepare new task
+        print(Fore.WHITE + Back.GREEN + Style.BRIGHT + "\n\nBob: I see you are interested in electric cars. Ask me any more quastion.")
+        tendency.append(current_tendency)
+    elif current_tendency == '2':
+        # Prepare new task
+        print(Fore.WHITE + Back.RED + Style.BRIGHT + "\n\nMark: I see you are not convinced by Bob. Let me show you why the Dacia Dustler is the best choice."
                                                       "\nOtherwise, if you want to exit, just type 'exit'.\n" + Style.RESET_ALL)
-
+        tendency.append(current_tendency)
+    else:
+        print(Fore.WHITE + Back.GREEN + Style.BRIGHT + "\n\nBob: Sorry for Mark, he is not our employee. Is there something else I can do for you?"
+                                                      "\nOtherwise, if you want to exit, just type 'exit'.\n" + Style.RESET_ALL)
     text_task = input()
 
 
@@ -146,3 +177,12 @@ while text_task.lower() != 'exit':
 '''##### End the conversation #####'''
 print(Fore.WHITE + Back.GREEN + Style.BRIGHT + "\n\nBob: Goodbye! Have a great day! I hope you will buy your dream electic car" + Style.RESET_ALL)
 print(Fore.WHITE + Back.RED + Style.BRIGHT + "\nMark: Goodbye! I hope you changed your mind and will buy a masculine car like the Dacia Dustler." + Style.RESET_ALL)
+
+
+# Save user tendency to a file
+with open("user_tendency.txt", "w") as file:
+    for item in tendency:
+        file.write(f"{item}\n")
+    file.write("\n\n")
+    file.close()
+    print("User tendency saved to 'user_tendency.txt'.")
